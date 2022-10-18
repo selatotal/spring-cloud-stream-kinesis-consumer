59b93d8dc2b4e26601b2b17d5ea60867bbfaa174
Tales Bitelo Viegas <selatotal@gmail.com>
2022-10-18 09:09:32 -0300

    Correção de imports

--- src/main/java/br/com/selat/springkinesisconsumer/consumer/KinesisConsumers.java
+++ src/main/java/br/com/selat/springkinesisconsumer/consumer/KinesisConsumers.java
@@ -1,6 +1,5 @@
 package br.com.selat.springkinesisconsumer.consumer;
 
-import br.com.selat.springkinesisconsumer.SpringCloudStreamKinesisConsumerApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
